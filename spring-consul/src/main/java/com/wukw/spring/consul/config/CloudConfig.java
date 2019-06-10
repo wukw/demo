f8489f042a9b8ce7684201c1ea507eb5332f906b
wukw <wukaiwei@dojo.com>
2019-06-10 15:07:36 +0800

    爬虫解析项目

---
+++ spring-consul/src/main/java/com/wukw/spring/consul/config/CloudConfig.java
@@ -0,0 +1,15 @@
+package com.wukw.spring.consul.config;
+
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
+
+@Configuration
+public class CloudConfig {
+    @Bean
+    @LoadBalanced
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
--- spring-consul/src/main/java/com/wukw/spring/consul/controller/TestController.java
+++ spring-consul/src/main/java/com/wukw/spring/consul/controller/TestController.java
@@ -1,9 +1,11 @@
 package com.wukw.spring.consul.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
 
 @RestController
 @RequestMapping("test")
@@ -11,8 +13,16 @@ public class TestController {
     @Value("${spring.cloud.consul.discovery.health-check-path}")
     String path;
 
+    @Autowired
+    RestTemplate restTemplate;
+
     @GetMapping("getPath")
     public String getPath() {
         return path;
+    }
+
+    @GetMapping("health")
+    public String health() {
+        return restTemplate.getForObject("http://consul-web-client/foo/actuator/health", String.class);
     }
 }
